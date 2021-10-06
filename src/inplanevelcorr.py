564e1d1144adcb2425ecdb87d3ce03ed2b3ff379
LiamABailey <bailey.liam102@gmail.com>
2021-10-06 19:28:58 -0400

    function definition and some test framing for correlation function

--- src/inplanevelcorr.py
+++ src/inplanevelcorr.py
@@ -2,3 +2,31 @@
 Module containing core methods for performing the measure of velocity correlation
  as a function of in-plane distance
 """
+
+
+def velocity_corr(data, radius_size):
+    """
+    Following the spatial correlation algorithm in Dombrowski et al. (2004),
+    calculate the spatial correlation (w/ additional metastatistics) for
+    the radius_size. Observes at 8 orientations at a pi/4 step.
+
+    Args
+    ----
+        data : np.ndarray
+            The data of interest, formatted as (n,m,2), where
+            [:,:,0] are the X velocities, and [:,:,1] are the Y velocities.
+
+        radius_size : 0 < int < min(n,m)
+            The size of the radius, in x/y grid coordinates
+
+    Returns
+    -------
+        float : the correlation score
+
+        int : the number of centers reviewed
+
+        int : the number of centers w/ 8 eligible surrounding data points
+
+        int : the number of centers w/ 4+ eligible surrounding data points
+
+    """
--- tests/test_inplanevelcorr.py
+++ tests/test_inplanevelcorr.py
@@ -1,3 +1,49 @@
 """
 Tests for inplanevelcorr.py
 """
+
+import unittest
+import velocitycorrelation2d.inplanevelcorr as vc
+
+
+class TestVelocityCorr(unittest.TestCase):
+
+    def test_velocity_corr(self):
+        """
+        Base test - checks
+        """
+
+    def test_velocity_corr_negativeradius(self):
+        """
+        OOB condition - what happens when the radius is less than 0
+        """
+
+    def test_velocity_corr_zeroradius(self):
+        """
+        OOB condition - what happens when the radius is zero
+        """
+
+    def test_velocity_corr_extremeradius(self):
+        """
+        OOB condition - what happens when the radius >= min(m,n)
+        """
+
+    def test_velocity_corr_oneradius(self):
+        """
+        Boundary condition - what happens when the radius = 1
+        """
+
+    def test_velocity_corr_maximalradius(self):
+        """
+        Boundary condition - what happens whin the radius = min(m,n) -1
+        """
+
+    def test_velocity_corr_wrongdatadim(self):
+        """
+        Data condition - process should check for 3 dimensions
+        """
+
+    def test_velocity_corr_wrongdepth(self):
+        """
+        Data condition - third dimension should be length 2
+        """
