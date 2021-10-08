65f8922cc6900b4aa98a510e50acb07a22272ad2
LiamABailey <bailey.liam102@gmail.com>
2021-10-07 20:49:36 -0400

    wrote spiral generator

--- tests/test_velocitycorrelation2D.py
+++ tests/test_velocitycorrelation2D.py
@@ -64,7 +64,7 @@ class TestVelocityCorr(unittest.TestCase):
 
     def test_velocity_corr_maximalradius(self):
         """
-        Boundary condition - what happens whin the radius = min(m,n) -1
+        Boundary condition - what happens when the radius = min(m,n) -1
         """
         radius = 9
         data = np.random.sample((10,10,2))
@@ -86,7 +86,7 @@ class TestVelocityCorr(unittest.TestCase):
         Data condition - process should check for 3 dimensions
         """
         radius = 1
-        data = np.random_sample((2,))
+        data = np.random.sample((2,))
         with self.assertRaises(TypeError):
             vc.velocity_corr(data, radius)
 
@@ -106,22 +106,53 @@ class TestVelocityCorr(unittest.TestCase):
         """
         Test that the program returns the proper high correlation
         """
+        radius = 1
+        data = np.ones((5,5,2)) + (np.random.sample((5,5,2,))-0.5)/50
+
+        corr, _, _, _ = vc.velocity_corr(data, radius)
+        self.assertGreater(corr, 0.9)
 
 
     def test_velocity_corr_nearzero(self):
         """
         Test that the program returns the proper near-zero correlation
         """
+        radius = 1
+        data = np.ones((10,10,2))
+        data[:,:,1] += (np.random.sample((10,10,))-0.5)/20
+
+        corr, _, _, _ = vc.velocity_corr(data, radius)
+        self.assertTrue(-0.1 < corr < 0.1, msg = f"Correlation {corr} is not between -0.1 and 0.1")
 
 
     def test_velocity_corr_low(self):
         """
         Test that the program returns the proper low correlation
         """
+        radius = 1
+        data =
+        corr, _, _, _, = vc.velocity_corr(data, radius)
+        self.assertLess(corr, -0.5)
 
+def _create_unit_spiral_array(radius):
+    """
+    Creates a collection of velocity vectors that form a spiral centered around
+    the middle of the matrix, where the final array shape is [(2*radius + 1), (2*radius + 1),2]
 
-    def test_velocity_corr_largeradius(self):
-        """
-        Test that the function works as expected when the radius is large
-        relative to the data provided
-        """
+    Args
+    ----
+        radius : int > 0
+
+    Returns
+    -------
+        np.array : The matrix containing spiral motion
+    """
+    dia = radius * 2 + 1
+    pos= np.zeros((dia, dia, 2))
+    pos[:,:,0] = np.broadcast_to(np.array(range(-radius, radius+1)), (dia,dia))
+    pos[:,:,1] = -1 * np.repeat(np.array([range(-radius, radius+1)]).T, dia, axis = 1)
+    spr = np.zeros_like(pos)
+    spr[:,:,0] = np.cos(np.arctan2(pos[:,:,1],pos[:,:,0]) - (np.pi/2))
+    spr[:,:,1] = np.sin(np.arctan2(pos[:,:,1],pos[:,:,0]) - (np.pi/2))
+    spr[radius,radius] = [0,0]
+    return spr
