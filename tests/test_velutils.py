e09219f2fab5a203a82d7b93cf99c5855ea79466
LiamABailey <bailey.liam102@gmail.com>
2021-10-14 14:43:15 -0400

    Added tests for square_input, additional test for rescale

--- tests/test_velutils.py
+++ tests/test_velutils.py
@@ -66,3 +66,75 @@ class TestRescalePositions(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             rescaled = u.rescale_positions(BASIC_FRAME, 1, 1)
+
+    def test_rescale_positions_integers(self):
+        """
+        Confirm that the position columns are retuned as integer types
+        """
+
+class TestSquareInput(unittest.TestCase):
+
+    def test_square_input(self):
+        """
+        basic test of square_input
+        """
+
+    def test_square_input_xcoord_col_ne(self):
+        """
+        test where provided xcoord_fea is not in the data frame
+        """
+
+    def test_square_input_ycoord_col_ne(self):
+        """
+        test where provided ycoord_fea is not in the data frame
+        """
+
+    def test_square_input_xvel_col_ne(self):
+        """
+        test where provided xvel_fea is not in the data frame
+        """
+
+    def test_square_input_yvel_col_ne(self):
+        """
+        test where provided yvel_fea is not in the data frame
+        """
+
+    def test_square_input_extra_columns(self):
+        """
+        test where extra columns are provided (shouldn't impact result)
+        """
+
+    def test_square_input_vel_nas(self):
+        """
+        Test to confirm that NA velocity values are retained
+        """
+
+    def test_square_input_xcoord_nas(self):
+        """
+        Confirms that NAs in the x-coordinate column leads to a raised error
+        """
+
+    def test_square_input_ycoord_nas(self):
+        """
+        Confirms that NAs in the y-coordinate column leads to a raised error
+        """
+
+    def test_square_input_xcoord_nonint(self):
+        """
+        Asserts error raised uf x-coord column isn't integer-based
+        """
+
+    def test_square_input_ycoord_nonint(self):
+        """
+        Asserts error raised uf y-coord column isn't integer-based
+        """
+
+    def test_square_input_xcoord_negative(self):
+        """
+        Asserts error raised if x-coord column contains negatives
+        """
+
+    def test_square_input_ycoord_negative(self):
+        """
+        Asserts error raised uf y-coord contains negatives
+        """
